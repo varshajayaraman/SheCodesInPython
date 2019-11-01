8968427b8be7e4b7307ff1d98d9dd9c4cefdde08
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2019-11-01 14:15:02 -0500

    Rearrange Alternate Positive and Negative

--- src/Controller.py
+++ src/Controller.py
@@ -2,4 +2,4 @@ import src.StringSearchWithAdjacentDifference
 import src.ArrangeAlternatePositiveNegative
 
 #src.StringSearchWithAdjacentDifference.strsearch([20,40,50,70,70,60], 20, 60)
-src.ArrangeAlternatePositiveNegative.arrange([-5, -2, 5, 2, 4, 7, 1, 8, 0, -8])
+src.ArrangeAlternatePositiveNegative.arrange([])
