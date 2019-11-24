7824b9ed5a0a376ffaf32610eb6c5e75526884ee
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2019-11-23 18:24:06 -0600

    Kth Largest Element in An Array using Minheap

---
+++ src/M215_KthLargestElementInAnArray.py
@@ -0,0 +1,13 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = nums[:k]
+        heapq.heapify(heap)
+        if len(nums)>k:
+            for i in range(k,len(nums)):
+                if nums[i]>heap[0]:
+                    heapq.heapreplace(heap, nums[i])
+        if len(heap)==0:
+            return 0
+        else:
+            return heap[0]
