ac01a51b0c84acddb08f9c8e8f7197adf47826ed
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2020-03-08 17:14:59 -0500

    Course Schedule : T:O(N*N) S: O(N)

--- src/M207_CourseSchedule.py
+++ src/M207_CourseSchedule.py
@@ -1,87 +1,35 @@
-def dfs(adjMat, visited, finished, i, j, row, col):
-        print("Visiting Node: "+str(i)+str(j))
-        # print("Status: "+str(visited[j]))
-        # print(visited)
-        # print(finished)
-        visited[i]=True
-        # print(finished)
-        if finished[j]:
-            # print("Glee")
-            finished[i] = True
-            return finished, True
-
-        else:
-            if not visited[j]:
-                # print("In")
-                visited[j] = True
-                # print(finished)
-                for c in range(col):
-                    # print(c)
-                    # print(finished)
-                    if adjMat[j][c] == 1:
-
-                        finished, res = dfs(adjMat, visited, finished, j, c, row, col)
-                        # print(res)
-                        if res==False:
-                            return finished, False
-                        else:
-                            # print("Glee1")
-                            finished[j]=True
-                            # finished[i]=True
-                            return finished, True
-                # print("Glee3")
-                finished[j]=True
-                # finished[i]=True
-                return finished, True
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        global adjDict, visited
+        adjDict = {}
+        for r in prerequisites:
+            if r[0] in adjDict:
+                adjDict[r[0]].append(r[1])
             else:
-                # print("Here")
-                return finished, False
+                adjDict[r[0]] = [r[1]]
 
+        print(adjDict)
 
-def canFinish(numCourses, prerequisites):
-
-        pr = prerequisites
-        adjMat = [[False for j in range(numCourses)] for i in range(numCourses)]
-        finished = [False for i in range(numCourses)]
-        visited = [False for i in range(numCourses)]
-        count = 0
-        row = len(adjMat)
-        if row == 0:
-            return True
-        col = len(adjMat[0])
-        nondep_courses = 0
-        for i in range(len(prerequisites)):
-            if len(pr[i]) > 1:
-                for j in range(len(pr[i])):
-                    course = pr[i][0]
-                    dep = pr[i][1]
-                    if adjMat[dep][course]:
-                        return False
-                    else:
-                        adjMat[course][dep] = True
+        def dfs(c):
+            global visited
+            if c not in adjDict:
+                return True
+            elif adjDict[c][0] in visited:
+                return False
             else:
-                nondep_courses+=1
-        print(adjMat)
-        flag=0
-        for i in range(len(adjMat)):
-            for j in range(len(adjMat[0])):
-                if adjMat[i][j] == 1:
-                    # print(finished)
-                    flag=1
-                    finished, res = dfs(adjMat, visited, finished, i, j, row, col)
-                    # print(res)
-                    if res == False:
+                for p in adjDict[c]:
+                    visited.add(p)
+                    if not dfs(p):
                         return False
-            if flag==1:
-                finished[i]=True
-                flag=0
-            print(finished)
+                    visited.remove(p)
+                return True
 
-        for x in finished:
-            if x:
-                count += 1
-                if (count+nondep_courses) == numCourses:
-                    return True
+        global visited
 
-        return False
+        for c in range(numCourses):
+            visited = set()
+            visited.add(c)
+            if not dfs(c):
+                return False
 
+        return True
