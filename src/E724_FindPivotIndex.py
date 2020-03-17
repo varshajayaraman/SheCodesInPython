983d9b9a8c51ea173a1b1caeeb1951f86b89f5e2
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2020-03-17 13:55:17 -0500

    Find Pivot Index: T: O(N) S: O(1)

--- src/E724_FindPivotIndex.py
+++ src/E724_FindPivotIndex.py
@@ -2,7 +2,6 @@ class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         ls = 0
         rs = sum(nums)
-
         for i in range(len(nums)):
             rs -= nums[i]
             if ls == rs:
