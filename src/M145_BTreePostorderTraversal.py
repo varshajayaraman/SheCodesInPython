349cd87dce64112e396bb6fafceba694ac16e341
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2020-11-24 10:20:34 -0800

    T:O(N) S:O(H)

---
+++ src/M145_BTreePostorderTraversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        global res
+        res = []
+        
+        def rec(root):
+            global res
+            if root.left:
+                rec(root.left)
+            if root.right:
+                rec(root.right)
+            res.append(root.val)
+        
+        if not root:
+            return res
+        rec(root)
+        return res
